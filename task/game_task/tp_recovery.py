a2c23da5554a9b1a79f7750b08da83bd5eb42204
MaoSan2006 <1653948415@qq.com>
2025-05-13 09:58:13 +0800

    优化了运行三月七助手前代码部分运行逻辑和顺序

--- task/game_task/tp_recovery.py
+++ task/game_task/tp_recovery.py
@@ -3,14 +3,16 @@ import pyautogui
 import time
 import logging
 from task.handler.auto_process_handler import auto_process_handler
+from task.game_task.monthly_card import monthly_card
 logger = logging.getLogger("tp_recovery")
 
 def tp_recovery():
     error_time = 0
-    while error_time <= 30:
-        #设置超时时间
-        if error_time == 30:
-            break
+    while error_time <= 10:
+        #月卡领取
+        if auto_process_handler.not_process("image/pre_march7th/monthly_card.png", 0.5) == True:
+            logger.info(f"月卡领取")
+            monthly_card()
         #打开地图
         elif auto_process_handler.not_process("image/tp_recovery/tp_map1.png", 0.2, confidence = 0.9)==False and auto_process_handler.not_process("image/tp_recovery/tp_map2.png", 0.2, confidence = 0.9) == False:
             logger.info(f"打开地图")
@@ -19,23 +21,25 @@ def tp_recovery():
         #终末视界关闭
         elif auto_process_handler.click("image/tp_recovery/end_visit.png", 0.2, confidence = 0.9) == True:
             logger.info(f"关闭终末视界")
-        #有可用锚点
-        elif auto_process_handler.click("image/tp_recovery/tp_place.png", 0.2, confidence = 0.9) == True:
-            logger.info(f"有可用锚点")
         #传送
         elif auto_process_handler.click("image/tp_recovery/tp.png", 0.2, confidence = 0.9) == True:
             logger.info(f"传送")
             return True
+        #有可用锚点
+        elif auto_process_handler.click("image/tp_recovery/taipusi_teleporter.png", 0.2, confidence = 0.9) == True:
+            logger.info(f"有可用锚点")
         #选择罗浮地图
         elif auto_process_handler.click("image/tp_recovery/luofu.png", 0.2, confidence = 0.9) == True:
             logger.info(f"选择罗浮地图")
             time.sleep(3)
         #选择长乐天
-        elif auto_process_handler.click("image/tp_recovery/clt.png", 0.2, confidence = 0.9) == True:
+        elif auto_process_handler.click("image/tp_recovery/taipusi.png", 0.2, confidence = 0.9) == True:
             logger.info(f"选择长乐天")
             time.sleep(1)
         #打开星轨地图
         elif auto_process_handler.click("image/tp_recovery/star_map1.png", 0.2, confidence = 0.9) == True or auto_process_handler.click("image/tp_recovery/star_map2.png", 0.2, confidence = 0.9) == True:
             logger.info(f"打开星轨地图")
             time.sleep(3)
-    return False
+    else:
+        logger.error(f"传送超时")
+        return False
